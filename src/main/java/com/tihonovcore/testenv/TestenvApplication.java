9d955ac2233314de963e25de93603a37b8bbde62
tihonovcore <tihonovcore@gmail.com>
2021-09-03 14:00:10 +0300

    init

---
+++ src/main/java/com/tihonovcore/testenv/TestenvApplication.java
@@ -0,0 +1,13 @@
+package com.tihonovcore.testenv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestenvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestenvApplication.class, args);
+	}
+
+}
